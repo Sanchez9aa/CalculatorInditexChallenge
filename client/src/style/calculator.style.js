f23f4ad5dacc173d20cc49ae43a7aa77e28bb65c
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-07 14:50:12 +0100

    Create calculator.style.js

---
+++ client/src/style/calculator.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledCalculator = styled.section`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  margin: auto;
+  background: ${props => props.theme === 1 ? "hsl(222, 26%, 31%)" : props.theme === 2 ? "hsl(0, 0%, 90%)" : "hsl(268, 75%, 9%)"};
+  transition: .5s linear;
+`
